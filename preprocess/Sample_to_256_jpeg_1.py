eab49b72997e185cfe78cb7ac86cda179cbe5d6c
Christopher Liao <cliao25@scc-wi3.scc.bu.edu>
2020-10-14 18:33:17 -0400

    Added handling of GCDM error from pydicom when preprocessing dicom files.

--- preprocess/Sample_to_256_jpeg_1.py
+++ preprocess/Sample_to_256_jpeg_1.py
@@ -28,78 +28,9 @@ import scipy
 import PIL
 import json
 
-class KagglePEDataset(torch.utils.data.Dataset):
-    """Kaggle PE dataset."""
-
-    def __init__(self, csv_file, root_dir, transform=None):
-        """
-        Args:
-            csv_file (string): Path to the csv file.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.pedataframe = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.transform = transform
-
-    def __len__(self):
-        """ Return number of 2D images. (Each CT slice is an independent image.)"""
-        return len(self.pedataframe)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = os.path.join(self.root_dir,
-                                self.pedataframe.StudyInstanceUID[idx],
-                                self.pedataframe.SeriesInstanceUID[idx],
-                                self.pedataframe.SOPInstanceUID[idx] + '.dcm')
-        dicom_image = pydicom.dcmread(img_name) 
-        image = dicom_image.pixel_array
-        
-        # in OSIC we find outside-scanner-regions with raw-values of -2000. 
-        # Let's threshold between air (0) and this default (-2000) using -1000
-        image[image <= -1000] = 0
-        
-        # convert to HU using DICOM information
-        # HU is a number between -1000 and 1000 (generally)
-        # good lung tissue is between -950 and -700 (approximately)
-        intercept = dicom_image.RescaleIntercept
-        slope = dicom_image.RescaleSlope
-        
-        if slope != 1:
-            image = slope * image.astype(np.float64)
-            
-        image = image.astype(np.int16)
-        image += np.int16(intercept)
-        
-        # Convert image from numpy array to PIL image (so that we can use pytorch transforms)
-        image[image >= 500] = 500
-        image[image <= -1000] = -1000
-        image = (image + 1000)/1500
-        image = image*255
-        image = np.uint8(image)
-        image = PIL.Image.fromarray(image).convert('RGB')
-
-        # image is 512x512 RGB PIL image
-        # pe_present_on_image is 0 or 1
-        sample = {'image': image, 
-                  'pe_present_on_image': int(self.pedataframe.pe_present_on_image[idx])}
-
-        # Only apply transform to image.
-        if self.transform:
-            sample['image'] = self.transform(sample['image'])
-            
-        return sample
-
 data_dir = '/projectnb/ece601/kaggle-pulmonary-embolism/rsna-str-pulmonary-embolism-detection/'
 train_csv = data_dir + 'train.csv'
 train_dir = data_dir + 'train/'
-
-
-# In[ ]:
-
 
 pedataframe = pd.read_csv(train_csv)
 
@@ -109,7 +40,14 @@ for idx in range(400000):
                             pedataframe.SeriesInstanceUID[idx],
                             pedataframe.SOPInstanceUID[idx] + '.dcm')
     dicom_image = pydicom.dcmread(img_name) 
-    image = dicom_image.pixel_array
+    
+    try:
+        # RuntimeError: The following handlers are available to decode the pixel ...
+        # data however they are missing required dependencies: GDCM (req. GDCM)
+        image = dicom_image.pixel_array
+    except:
+        print('Error parsing ', img_name)
+        continue
         
     # in OSIC we find outside-scanner-regions with raw-values of -2000. 
     # Let's threshold between air (0) and this default (-2000) using -1000
@@ -154,11 +92,3 @@ for idx in range(400000):
     image.save(out_filename)
     if idx % 10000 == 0:
         print(idx)
-                             
-
-
-# In[ ]:
-
-
-
-
