0296f7ca4d30de2e6e6c49416faabfadc34fd515
Megan Parsons <meganparsons@alumni.harvard.edu>
2020-11-02 16:21:13 -0500

    Update for Kaggle input

--- PENet/test_from_dicom.py
+++ PENet/test_from_dicom.py
@@ -7,7 +7,7 @@ import torch
 from saver import ModelSaver
 
 
-def main(args):
+def main(args): 
 
     # create npy from dicom
     print("Reading input dicom...")
@@ -15,7 +15,7 @@ def main(args):
 
     # normalize and convert to tensor
     print("Formatting input for model...")
-    study_windows = util.format_img(study)
+    study_windows = util.format_img(study) 
 
     print ("Loading saved model...")
     model, ckpt_info = ModelSaver.load_model(args.ckpt_path, args.gpu_ids)
@@ -28,20 +28,20 @@ def main(args):
     model.eval()
     predicted_probabilities = []    # window wise for a single study
     with torch.no_grad():
-        for window in study_windows:
+        for window in study_windows: 
             cls_logits = model.forward(window.to(args.device, dtype=torch.float))
             cls_probs = torch.sigmoid(cls_logits).to('cpu').numpy()
             predicted_probabilities.append(cls_probs[0][0])
 
     print (f"Probablity of having Pulmonary Embolism: {max(predicted_probabilities)}")
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
 
     # parse in arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_study", type=str, default="/projectnb/ece601/kaggle-pulmonary-embolism/rsna-str-pulmonary-embolism-detection/test/00268ff88746/75d23269adbd")
-    parser.add_argument("--series_description", type=str, default="CTPA")
-    parser.add_argument("--ckpt_path", type=str, default="/projectnb/ece601/kaggle-pulmonary-embolism/meganmp/ckpts/penet_best.pth.tar")
+    parser.add_argument("--input_study", type=str, default="/data4/intermountain/CTPA/CTPA_RANDOM_DICOM/1770659")
+    parser.add_argument("--series_description", type=str, default="CTA 2.0 CTA/PULM CE")
+    parser.add_argument("--ckpt_path", type=str, default="/data4/PE_stanford/ckpts/best.pth.tar")
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--gpu_ids", type=str, default="0")
     args = parser.parse_args()
@@ -51,4 +51,4 @@ if __name__ == "__main__":
     else:
         args.gpu_ids = [int(args.gpu_ids)]
 
-    main(args)
+    main(args) 
