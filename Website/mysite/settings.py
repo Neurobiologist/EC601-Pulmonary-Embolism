45cde0ce675bb836ceebe5983b0bc3777984d69c
Megan Parsons <meganparsons@alumni.harvard.edu>
2020-12-15 03:03:34 -0500

    Cleanup files

--- Website/mysite/settings.py
+++ Website/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nx(w8yfqj&1(wiqdo)uc35#ps#ef+iz0!ua@%c4=78s2#53t!y'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
